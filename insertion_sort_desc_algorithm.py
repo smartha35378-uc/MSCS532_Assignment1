f434a58a6561b869232299856b70e27788ff5209
smartha-vctg <smartha@vivacitytech.com>
2026-01-18 12:52:18 -0600

    Add user input option to insertion sort

--- insertion_sort_desc_algorithm.py
+++ insertion_sort_desc_algorithm.py
@@ -1,10 +1,9 @@
 def insertion_sort_desc(arr):
-    # This Algorithm Sorts given arr in monotonically decreasing order (largest to smallest)
+    # This Algorithm 3 4Sorts arr in monotonically decreasing order
     for j in range(1, len(arr)):
         key = arr[j]
         i = j - 1
 
-        # Move elements that are smaller than key to one position ahead
         while i >= 0 and arr[i] < key:
             arr[i + 1] = arr[i]
             i -= 1
@@ -14,7 +13,9 @@ def insertion_sort_desc(arr):
 
 
 if __name__ == "__main__":
-    data = [5, 2, 9, 1, 5, 6]
+    user_input = input("Enter numbers separated by spaces: ")
+    data = list(map(int, user_input.split()))
+
     print("Original Array:", data)
     insertion_sort_desc(data)
     print("Sorted Array(decreasing):", data)
